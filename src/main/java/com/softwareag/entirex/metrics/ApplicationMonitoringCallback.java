355243079c3b722649cd5fb31f19a8158095d33c
Thomas Rupp <Thomas.Rupp1@ibm.com>
2025-06-05 08:24:12 +0200

    RPC metrics implemented

--- src/main/java/com/softwareag/entirex/metrics/ApplicationMonitoringCallback.java
+++ src/main/java/com/softwareag/entirex/metrics/ApplicationMonitoringCallback.java
@@ -42,9 +42,9 @@ public class ApplicationMonitoringCallback implements DataCollectorCallback {
 			rpcTimeDBCalls              = Gauge.build().name  ( labelPrefix + "time_db_calls"               ).help( "The time spent for database calls  microseconds"                                                                                    ).labelNames( "broker", "service", "program" ).register();
 			rpcTimeDBTransport          = Gauge.build().name  ( labelPrefix + "time_db_transport"           ).help( "The transport time from the Natural user program to the Adabas router and back including the client receiving time in microseconds" ).labelNames( "broker", "service", "program" ).register();
 			
-			rpcCalls                    = Counter.build().name  ( labelPrefix + "calls"           ).help( "Count RPC calls"         ).labelNames( "broker", "service", "program" ).register();
-			rpcCallsSuccess             = Counter.build().name  ( labelPrefix + "success_calls"   ).help( "Count RPC success calls" ).labelNames( "broker", "service", "program" ).register();
-			rpcCallsError               = Counter.build().name  ( labelPrefix + "error_calls"     ).help( "Count RPC error calls"   ).labelNames( "broker", "service", "program" ).register();
+			rpcCalls                    = Counter.build().name( labelPrefix + "calls"           ).help( "Count RPC calls"         ).labelNames( "broker", "service", "program" ).register();
+			rpcCallsSuccess             = Counter.build().name( labelPrefix + "success_calls"   ).help( "Count RPC success calls" ).labelNames( "broker", "service", "program" ).register();
+			rpcCallsError               = Counter.build().name( labelPrefix + "error_calls"     ).help( "Count RPC error calls"   ).labelNames( "broker", "service", "program" ).register();
 		}
 		catch (Throwable e ) {
 			logger.error( "On ApplicationMonitoringCallback init: ", e );
@@ -59,9 +59,73 @@ public class ApplicationMonitoringCallback implements DataCollectorCallback {
 
 	@Override
 	public void processEvent(Map<String, String> attributes) throws Exception {
-		String scenario = attributes.get( "AppMonScenario");
+		String scenario = attributes.get( "AppMonScenario" );
 		if ( scenario != null && scenario.equals( "RPC" ) ) {
-			
+			//Copy metrics from DC to Prometheus
+			String broker  = getLabelBroker( attributes );
+			String service = getLabelService( attributes );
+			String program = getLabelProgram( attributes );
+
+			rpcCalls.labels( broker, service, program ).inc();
+			if ( hasError( attributes ) ) 
+				rpcCallsError.labels( broker, service, program ).inc();
+			else
+				rpcCallsSuccess.labels( broker, service, program ).inc();
+
+			String valueS = attributes.get( "TimeResponse" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeResponse.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeClientLayer" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeClientLayer.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeClientTransport" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeClientTransport.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeBroker" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeBroker.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeBrokerWaitForServer" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeBrokerWaitForServer.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeServerTransport" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeServerTransport.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeServerLayer" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeServerLayer.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeServerProgram" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeServerProgram.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeDBCalls" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeDBCalls.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
+			valueS = attributes.get( "TimeDBTransport" );
+			if ( valueS != null && valueS.length() > 0 )
+				rpcTimeDBTransport.labels( broker, service, program ).inc( Double.valueOf( valueS ) );
 		}
+	}
+	private String getLabelBroker( Map<String, String> attributes ) {
+		String back = attributes.get( "Address" );
+		int i = back.indexOf( "/" );
+		return back.substring( 0, i );
+	}
+	private String getLabelService( Map<String, String> attributes ) {
+		String back = attributes.get( "ApplicationName" );
+		if ( back.startsWith( "RPC" ) ) {
+			int i = back.indexOf( "/" );
+			if ( i > 0 ) {
+				int j = back.indexOf( "/", i + 1 );
+				if ( j > 0 )
+					back = back.substring( i + 1, j );				
+			}
+		}
+		return back;
+	}
+	
+	private String getLabelProgram( Map<String, String> attributes ) {
+		return attributes.get( "Program" );
+	}
+	private boolean hasError( Map<String, String> attributes ) {
+		return attributes.get( "ErrorCode" ) != null;
 	}
 }
